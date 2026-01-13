b13f91f4c79b9aa1a5c8abcf0d76b8b823c871fd
RealTaleYT <realtaleyt@gmail.com>
2026-01-13 10:25:47 +0100

    1. Creado proyecto Open Weather

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,5 +8,6 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
+  
   constructor() {}
 }
